6bfe4efae783fb70d0d020377c370a3ab35c23a5
Kavita Thomas <156891537+kavitaithomas@users.noreply.github.com>
2026-03-09 21:31:23 -0300

    feat: :sparkles: simple navbar added

--- components/ui/NavBar.tsx
+++ components/ui/NavBar.tsx
@@ -4,8 +4,8 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="flex items-center justify-between p-4">
-      <Link href="/" className="font-bold text-xl">
-        Logo
+      <Link href="/">
+        <Image src="/logo.svg" width={100} height={50} alt="HALII logo" />
       </Link>
       <div className="flex gap-4">
         <Link href="/">Home</Link>
